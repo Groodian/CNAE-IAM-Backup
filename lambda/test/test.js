4a5c9e3879f4039354d632ee0b4f7a139d01d902
Tom Marek <tom.marek@stud.h-da.de>
2023-06-08 19:51:05 +0200

    Added kubernetes gateway test

--- lambda/test/test.js
+++ lambda/test/test.js
@@ -2,7 +2,7 @@ const jwt_decode = require('jwt-decode');
 
 exports.handler = async (event) => {
     console.log(JSON.stringify(event))
-    const tokenDecode = jwt_decode(event.headers.Authorization);
+    const tokenDecode = jwt_decode(event.headers.authorization);
 
     return  {
         statusCode: 200,
