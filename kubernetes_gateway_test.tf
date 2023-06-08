188b7efe4b0897666201778cfb6a5ffffd7e88ad
Tom Marek <tom.marek@stud.h-da.de>
2023-06-08 18:49:36 +0200

    Added kubernetes gateway test

---
+++ kubernetes_gateway_test.tf
@@ -0,0 +1,97 @@
+data "terraform_remote_state" "infrastructure_state" {
+  backend = "http"
+
+  config = {
+    address  = "https://code.fbi.h-da.de/api/v4/projects/29127/terraform/state/aws_infrastructure_state"
+    username = "${var.INFRASTRUCTURE_ACCESS_TOKEN_USERNAME}"
+    password = "${var.INFRASTRUCTURE_ACCESS_TOKEN}"
+  }
+}
+
+variable "INFRASTRUCTURE_ACCESS_TOKEN" {
+  description = "Value of $INFRASTRUCTURE_ACCESS_TOKEN from gitlab-ci"
+  type        = string
+}
+
+variable "INFRASTRUCTURE_ACCESS_TOKEN_USERNAME" {
+  description = "Value of $INFRASTRUCTURE_ACCESS_TOKEN_USERNAME from gitlab-ci"
+  type        = string
+}
+
+# Retrieve information about an EKS Cluster.
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.infrastructure_state.outputs.cluster_name
+}
+
+# Get an authentication token to communicate with an EKS cluster.
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.infrastructure_state.outputs.cluster_name
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type"     = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-internal" = "true"
+    }
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
--- main.tf
+++ main.tf
@@ -17,9 +17,23 @@ terraform {
       source  = "hashicorp/archive"
       version = "~> 2.2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.id]
+    command     = "aws"
+  }
 }
